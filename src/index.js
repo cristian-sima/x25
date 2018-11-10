d9be8074c673e433b5225fa2e257a31777832a7c
Cristian Sima <cristian.sima93@gmail.com>
2018-11-10 21:08:24 +0200

    FIX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 // @flow
-
-import utility from "./utility/index.js";
-
-const trula = {
-  banana: "exista o banana",
-};
-
-export {
-  utility,
-  trula,
-};
--- src/utility/index.js
+++ src/utility/index.js
@@ -1,23 +1,10 @@
 // @flow
 
-import * as calendar from "./calendar";
-import * as date from "./date";
-import * as language from "./language";
-import * as normalize from "./normalize";
-import * as numbers from "./numbers";
-import * as others from "./others";
-import * as strings from "./strings";
-import * as validation from "./validation";
-
-const all = {
-  ...calendar,
-  ...date,
-  ...language,
-  ...normalize,
-  ...numbers,
-  ...others,
-  ...strings,
-  ...validation,
-};
-
-export default all;
+export * from "./calendar";
+export * from "./date";
+export * from "./language";
+export * from "./normalize";
+export * from "./numbers";
+export * from "./others";
+export * from "./strings";
+export * from "./validation";
