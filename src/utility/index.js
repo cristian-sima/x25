3e7356621889f56bcccaf0671df10e69b3a7299c
Cristian Sima <cristian.sima93@gmail.com>
2018-11-10 22:08:39 +0200

    fix

--- src/utility/index.js
+++ src/utility/index.js
@@ -5,6 +5,7 @@ export * from "./date";
 export * from "./language";
 export * from "./normalize";
 export * from "./numbers";
+export * from "./numeric";
 export * from "./others";
 export * from "./strings";
 export * from "./validation";
