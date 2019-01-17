b2896568a73cd56341f2bd859aed3c4aa0b30bbb
Cristian Sima <cristian.sima93@gmail.com>
2019-01-17 22:30:25 +0200

    1.6.3

--- src/initApplication.js
+++ src/initApplication.js
@@ -14,7 +14,30 @@ const perform = (configuration) => {
   if (process.env.NODE_ENV === "production") {
     const Sentry = require("@sentry/browser");
 
-    Sentry.init(configuration.sentry);
+    Sentry.init({
+      ...configuration.sentry,
+      beforeSend (event) {
+        if (event.exception) {
+          Sentry.showReportDialog({
+            title     : "Se pare că avem probleme.",
+            subtitle  : "Echipa noastră a fost anunțată.",
+            subtitle2 : `Dacă doriți să ne ajutați, spuneți-ne ce sa întâmplat mai jos.
+            Ne va ajuta foarte mult`,
+            labelName     : "Numele dvs.",
+            labelEmail    : "Adresa de e-mail",
+            labelComments : "Ce s-a întâmplat?",
+            labelClose    : "Închide",
+            labelSubmit   : "Trimite",
+            errorGeneric  : `A apărut o eroare necunoscută la trimiterea raportului
+             dvs. Vă rugăm să încercați din nou.`,
+            errorFormEntry : "Unele câmpuri au fost nevalide. Corectați erorile și încercați din nou.",
+            successMessage : "Feedback-ul dvs. a fost trimis. Mulțumesc!",
+          });
+        }
+
+        return event;
+      },
+    });
   } else {
     setTimeout(() => {
       setFavIconToDev();
