d4ce328b0c18833e068e59e3ce0a4c106f1878de
Cristian Sima <cristian.sima93@gmail.com>
2019-01-31 19:24:30 +0200

    Update index.jsx

--- src/async/index.jsx
+++ src/async/index.jsx
@@ -1,18 +1,19 @@
 // @flow
 /* eslint-disable new-cap, react/prefer-stateless-function, react/require-optimization */
 
-// type injectPaginatorTypes = {
-//   key: string,
-//   itemsReducer: any;
-//   pagesReducer: any;
-// };
+type injectPaginatorTypes = {
+  key: string,
+  itemsReducer: any;
+  pagesReducer: any;
+};
 
 import Loadable from "react-loadable";
 import React from "react";
 
 import RouteLoading from "./RouteLoading";
-// import { injectReducer } from "redux-injector";
-import InitModule from "./InitModule";
+import { injectReducer } from "redux-injector";
+// import InitModule from "./InitModule";
+import { injectModals } from "../Modal/util";
 
 const
   timeout = 15000;
@@ -22,42 +23,42 @@ export let
     <div>{"No ErrorBoundary passed to x25"}</div>
   );
 
-// const injectPaginator = ({ key, itemsReducer, pagesReducer } : injectPaginatorTypes) => {
-//   injectReducer(`entities.${key}`, itemsReducer);
-//   injectReducer(`paginations.${key}`, pagesReducer);
-// };
+const injectPaginator = ({ key, itemsReducer, pagesReducer } : injectPaginatorTypes) => {
+  injectReducer(`entities.${key}`, itemsReducer);
+  injectReducer(`paginations.${key}`, pagesReducer);
+};
 
-// const renderWithReducer = (route, props) => {
-//   const { Component, reducers, modals, paginators } = route.default;
-//
-//   if (reducers) {
-//     if (Array.isArray(reducers)) {
-//       for (const { key, func } of reducers) {
-//         injectReducer(key, func);
-//       }
-//     } else {
-//       const { key, func } = reducers;
-//
-//       injectReducer(key, func);
-//     }
-//   }
-//
-//   if (modals) {
-//     injectModals(modals);
-//   }
-//
-//   if (paginators) {
-//     if (Array.isArray(paginators)) {
-//       for (const paginator of paginators) {
-//         injectPaginator(paginator);
-//       }
-//     } else {
-//       injectPaginator(paginators);
-//     }
-//   }
-//
-//   return <Component {...props} />;
-// };
+const renderWithReducer = (route, props) => {
+  const { Component, reducers, modals, paginators } = route.default;
+
+  if (reducers) {
+    if (Array.isArray(reducers)) {
+      for (const { key, func } of reducers) {
+        injectReducer(key, func);
+      }
+    } else {
+      const { key, func } = reducers;
+
+      injectReducer(key, func);
+    }
+  }
+
+  if (modals) {
+    injectModals(modals);
+  }
+
+  if (paginators) {
+    if (Array.isArray(paginators)) {
+      for (const paginator of paginators) {
+        injectPaginator(paginator);
+      }
+    } else {
+      injectPaginator(paginators);
+    }
+  }
+
+  return <Component {...props} />;
+};
 
 export const setErrorBoundary = (theError) => {
   ErrorBoundary = theError;
@@ -66,6 +67,6 @@ export const setErrorBoundary = (theError) => {
 export const createAsyncRoute = (loader) => Loadable({
   loader,
   loading : RouteLoading,
-  render  : InitModule,
+  render  : renderWithReducer,
   timeout,
 });
