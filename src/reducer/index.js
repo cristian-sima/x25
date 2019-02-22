7e78d0b247057589da3c3a116799578c29bff2be
Cristian Sima <cristian.sima93@gmail.com>
2019-02-22 17:57:14 +0200

    fix packages

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,15 +2,11 @@
 
 import modal, { selectors as modalSelectors } from "../Modal/reducer";
 import account, { selectors as accountSelectors } from "../Account/reducer";
-// import board from "../Board/reducer";
 
 import auth, { selectors as authSelectors } from "./auth";
 import module from "./module";
 
-import immutableRouting from "./immutable-routing";
-
 const state = {
-  // board,
   module,
   account,
   auth,
@@ -26,5 +22,4 @@ const selectors = {
 export {
   state,
   selectors,
-  immutableRouting,
 };
